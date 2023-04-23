081d05fd430947c32874755c0077729c28a54077
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-04-23 07:00:20 +0530

    feat: set initial data to intersection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useMemo, useRef, useState } from "react";
 function App() {
+  const targetRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries; // const entry = entries[0];
+    setIsVisible(entry.isIntersecting);
+  };
+  const options = useMemo(() => {
+    return {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    };
+  }, []);
 
   return (
     <>
       <h1 className="header">
-        {isVisible ? "Not in viewport" : "In viewport"}
+        {!isVisible ? "Not in viewport" : "In viewport"}
       </h1>
+      <div className="gap"></div>
+      <img
+        alt="intersection"
+        src="src/assets/intersection.png"
+        ref={targetRef}
+      />
     </>
   );
 }
