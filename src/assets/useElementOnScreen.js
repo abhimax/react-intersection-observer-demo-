0ebf2e1f6d34baf9580a94c791c7be84612a842b
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-04-25 23:50:18 +0530

    feat: add custom hook

---
+++ src/assets/useElementOnScreen.js
@@ -0,0 +1,30 @@
+import { useEffect, useMemo } from "react";
+
+const useElementOnScreen = (options, targetRef) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries; // const entry = entries[0];
+    setIsVisible(entry.isIntersecting);
+    console.log("callback!!");
+  };
+  const options = useMemo(() => {
+    return {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect!!");
+    const observer = new IntersectionObserver(callbackFunction, options);
+    const currentTarget = targetRef.current;
+    if (currentTarget) observer.observe(currentTarget);
+
+    return () => {
+      if (currentTarget) observer.unobserve(currentTarget);
+    };
+  }, [targetRef, options]);
+};
+export default useElementOnScreen;
